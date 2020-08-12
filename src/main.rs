56084e966b0558cd04644db0ef63ffd8198b3bb2
DefunctLizard <grantshandy@gmail.com>
2020-08-12 17:20:59 -0600

    add pages

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,44 @@ use gio::prelude::*;
 use std::env::args;
 
 fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
+	let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
 
-    window.set_title("Finance");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(1050, 650);
+	let stack = gtk::Stack::new();
+	stack.set_transition_type(gtk::StackTransitionType::SlideLeftRight);
+	stack.set_transition_duration(1000);
+	
+	let value_button = gtk::Button::new();
+	let graph_button = gtk::Button::new();
+	
+	stack.add_titled(&value_button, "value_page", "Stock Value");
+	stack.add_titled(&graph_button, "graph_page", "Stock Graph");
+	let stack_switcher = gtk::StackSwitcher::new();
+	stack_switcher.set_stack(Some(&stack));
 
-//    let headerbar = gtk::HeaderBar::set_title("Finance");
+	let headerbar = gtk::HeaderBar::new();
+	headerbar.set_title(Some("Finance"));
+	headerbar.set_show_close_button(true);
+	headerbar.set_custom_title(Some(&stack_switcher));
 
-    window.show_all();
+	let window = gtk::ApplicationWindow::new(application);
+	window.set_title("Finance");
+	window.set_icon_name(Some("org.skylinecc.finance"));
+	window.set_border_width(10);
+	window.set_titlebar(Some(&headerbar));
+	window.set_position(gtk::WindowPosition::Center);
+	window.set_default_size(1050, 650);
+	
+        window.add(&stack);
+	window.show_all();
 }
 
 fn main() {
-    let application = gtk::Application::new(Some("com.skylinecc.finance"), Default::default())
-        .expect("Initialization failed...");
+	let application = gtk::Application::new(Some("com.skylinecc.finance"), Default::default())
+		.expect("Initialization failed...");
+		
+	application.connect_activate(|app| {
+		build_ui(app);
+	});
 
-    application.connect_activate(|app| {
-        build_ui(app);
-    });
-
-    application.run(&args().collect::<Vec<_>>());
+	application.run(&args().collect::<Vec<_>>());
 }
