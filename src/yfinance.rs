a692ada175447600ab5d1ea48f91feead5bbf0f5
DefunctLizard <grantshandy@gmail.com>
2020-08-18 20:56:33 -0600

    nightly update

--- src/yfinance.rs
+++ src/yfinance.rs
@@ -1,17 +1,16 @@
-use yahoo_finance_api as yahoo;
-use tokio_test;
+pub fn get_quote() -> &'static str {
+//	let provider = yahoo::YahooConnector::new();
+	// get the latest quotes in 1 minute interval
 
-pub fn get_quote() -> std::string::String {
-    let provider = yahoo::YahooConnector::new();
-    // get the latest quotes in 1 minute interval
+//	let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1m")).unwrap();
+	//extract just the latest valid quote summery
+//	let quote = response.last_quote().unwrap();
 
-    let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1m")).unwrap();
-    // extract just the latest valid quote summery
-    let quote = response.last_quote().unwrap();
+//	let quote = quote.close;
 
-    let quote = quote.close;
+//	let quote_close = format!("{:.2}", quote);
 
-    let quote_close = format!("{:.2}", quote);
+	let quote_current = "400";
 
-    return quote_close;
+	return quote_current;
 }
